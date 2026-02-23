6811a067229265c031e062e1a92a3176e084f961
AherAshwini <aherashwini25@gmail.com>
2026-02-23 18:07:55 +0200

    setup and requirement

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str) -> List[str]:
+    '''
+    This function will return the list of libraries mentioned in requirements.txt
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+
+setup(
+    name = 'Speech Channel Quality',
+    version = '0.0.1',
+    author = 'Ashwini',
+    author_email = 'aherashwini25@gmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
